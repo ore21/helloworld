f909750c89b66c37094e07ef97fd441e6febcf46
Brandon <bgonzalez526@students.mchenry.edu>
2018-08-23 11:21:59 -0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        String aString = "Brandon";
+        System.out.println("My name is " + aString);
+    }
+}
+
